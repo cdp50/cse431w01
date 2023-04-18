592642b5f6f6023a1be932193f9272e55c31d8b2
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-04-18 10:55:04 -0600

    done2.0

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const routes = require('express').Router();
 const myController = require('../controllers');
 
+routes.get('/', function (req, res) {
+    res.send('Hello World');
+ })
+
 routes.get('/awesome', myController.awesome);
 routes.get('/superAwesome', myController.superAwesome);
 
--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
-
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
